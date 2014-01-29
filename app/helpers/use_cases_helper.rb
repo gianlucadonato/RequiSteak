02ad2064a8a31369d2865943bddc8bc37334d12f
gianlucadonato <gian.dnt@gmail.com>
2014-01-29 04:50:54 +0100

    minor changes in uc layout

--- app/helpers/use_cases_helper.rb
+++ app/helpers/use_cases_helper.rb
@@ -3,17 +3,12 @@ module UseCasesHelper
 		uc.map do |use_case, sub_uc|
 			content_tag(:div,
 				content_tag(:div,
-					content_tag(:dl, 
-						content_tag(:dd, content_tag(:a, @prefix + use_case.hierarchy, href: "#panel#{use_case.id}", class: "orange"), class: "active") +
-						content_tag(:dd, content_tag(:a, "Requirements", href: "#panel-req", class: "green")),
-					class: "tabs", data: { tab: "" }) +
-					content_tag(:div, 
-						content_tag(:div, content_tag(:p, content_tag(:a, capture do link_to use_case.title, use_case_path(use_case) end) + 
-							content_tag(:p, use_case.description, class: "nested_parag")),class: "content active", id: "panel#{use_case.id}") +
-						content_tag(:div, content_tag(:p, "world"),class: "content", id: "panel-req"),
-					class: "tabs-content"),
-				class: "use_case"),
-		class: "panel") +
+					link_to(@prefix + use_case.hierarchy, use_case_path(use_case), class: "orange treelabel") + 
+						content_tag(:label, 
+							content_tag(:strong, "\  " + use_case.title.split(" ")[2..-1].join(" ")),
+						class: "nested_label") +
+					content_tag(:p, use_case.description, class: "nested_p")),
+				class: "use_case panel") +
 	    
 			content_tag(:div, nested_usecases(sub_uc), :class => "nested_requirements")
 		end.join.html_safe
