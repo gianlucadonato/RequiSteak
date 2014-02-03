99f2d441fecb0211fa36ee829be93caceda8d125
gianlucadonato <gian.dnt@gmail.com>
2014-02-03 15:43:56 +0100

    removed export compontents from index

--- config/routes.rb
+++ config/routes.rb
@@ -20,12 +20,12 @@ Requisteak::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
-  get '/downloads'    => 'download#index'
-  get '/downloads/back-end'    => 'download#export_backend'
-  get '/downloads/front-end'    => 'download#export_frontend'
+  get '/downloads'             => 'download#index'
+  get '/downloads/back-end'    => 'download#export_backend', as: 'downloads_backend'
+  get '/downloads/front-end'   => 'download#export_frontend', as: 'downloads_frontend'
 
-  get '/export_requirements'    => 'requirements#export'
-  get '/export_use_cases'    => 'use_cases#export'
+  get '/export_requirements'   => 'requirements#export'
+  get '/export_use_cases'      => 'use_cases#export'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
