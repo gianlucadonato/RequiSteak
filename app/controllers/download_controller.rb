1d97f2fda7088ae02087d8e09965c6d15b088a63
gianlucadonato <gian.dnt@gmail.com>
2014-02-03 16:20:39 +0100

    add frontend_export

--- app/controllers/download_controller.rb
+++ app/controllers/download_controller.rb
@@ -8,15 +8,15 @@ class DownloadController < ApplicationController
 file = "backend-packages.tex"
     File.open(file, "wb+"){ |f|
       f << "\\subsection{Packages}"
-     	comp = []
+     	back_comp = []
       backend = Component.find_by_title("Back-end");
       if !backend.nil?
-        comp << backend
+        back_comp << backend
         backend.descendants.each do |c|
-          comp << c
+          back_comp << c
         end
       end
-comp.each do |comp|      
+back_comp.each do |comp|      
   f << "
   \\subsubsection{#{comp.title}}
   \\paragraph{Informazioni sul package} 
@@ -107,11 +107,116 @@ comp.each do |comp|
         end
     end
   end
-end #end comp.each
+end #end back_comp.each
     }
     send_file(file)
 	end #end export_backend
 
 	def export_frontend
+file = "front-packages.tex"
+    File.open(file, "wb+"){ |f|
+      f << "\\subsection{Packages}"
+     	front_comp = []
+      frontend = Component.find_by_title("Front-end");
+      if !frontend.nil?
+        front_comp << frontend
+        frontend.descendants.each do |c|
+          front_comp << c
+        end
+      end
+front_comp.each do |comp|      
+  f << "
+  \\subsubsection{#{comp.title}}
+  \\paragraph{Informazioni sul package} 
+
+    \\begin{figure}[H] 
+      \\begin{center} 
+        %\\includegraphics[scale=1]{packages/NomePackage.png}  
+        \\caption{Componente #{comp.title}}
+      \\end{center}  
+    \\end{figure} 
+
+  \\subparagraph{Descrizione} 
+    \\begin{itemize}
+    \\item[] #{comp.description}
+    \\end{itemize} "
+
+  if !comp.descendants.empty?
+  f << "
+    \\subparagraph{Package contenuti} 
+    \\begin{itemize}"
+    comp.descendants.each do |pkg|  
+      f << "
+        \\item #{pkg.title}" 
+    end
+  f << "
+    \\end{itemize}"
+  end
+
+  if !comp.packages.empty?  
+  f<< "
+  \\subparagraph{Interazioni con altri componenti} 
+    \\begin{itemize} "
+    comp.packages.each do |pkg| 
+      f << "
+        \\item #{pkg.title}" 
+    end
+  f << "  
+    \\end{itemize} "
+  end
+
+  if !comp.units.empty? 
+    f<< "
+    \\paragraph{Classi}"
+    comp.units.each do |u| 
+      f << "
+      \\subparagraph{#{u.title}}
+        
+        \\textbf{\\\\ \\\\ Descrizione} 
+          \\begin{itemize}
+            \\item[] #{u.description}
+          \\end{itemize}      
+        \\textbf{Utilizzo}  
+          \\begin{itemize}
+            \\item[] #{u.use}
+          \\end{itemize}" 
+        if !u.ancestors.empty?
+        f << "
+          \\textbf{Classi Ereditate}
+          \\begin{itemize}"
+            u.ancestors.each do |p|
+              f << "
+                \\item{#{p.title}}"
+            end
+        f << "
+          \\end{itemize}"
+        end
+        if !u.descendants.empty?  
+        f << "
+          \\textbf{Classi Figlie}
+          \\begin{itemize}"
+            u.descendants.each do |c|
+              f << "
+              \\item{#{c.title}}"
+            end
+        f << "
+          \\end{itemize}"
+        end
+        if !u.units.empty?  
+        f << "
+          \\textbf{Relazioni con altre classi}
+          \\begin{itemize}"
+            u.units.each do |c|
+              f << "
+              \\item{#{c.title}}"
+            end
+        f << "
+          \\end{itemize}"
+        end
+    end
+  end
+end #end front_comp.each
+    }
+    send_file(file)
 	end #end export_frontend
 end
